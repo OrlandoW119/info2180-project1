8236ad7ffd330b7ee46da4cdd398d15c16530e53
Orlando Williams <Kingorlando123@yahoo.com>
2020-11-04 19:00:36 -0500

    Edited README.md

--- scripts.js
+++ scripts.js
@@ -1,16 +1,19 @@
 /* Add your JavaScript to this file */
 window.onload=function(){
-  var Subbutton=document.getElementByTagName("button")[0];
-  Subbutton.addEventListener("click",SubbuttonMsg);
-  
-  function SubbuttonMsg() {
-    if (document.getElementById("email").innerHTML.length == 0) {
-        var message = document.getElementsByClassName('message')[0];
-        message.innerHTML += "Please enter a valid email address";}
-    else {
-        var message = document.getElementsByClassName('message')[0];
-        var newemail =document.getElementById("email").innerHTML;
-        message.innerHTML += "Thank you! Your email address ${newemail} has been added to our mailing list!";
-    }
-  }
-}
+ document.addEventListener('DOMContentLoaded', function() {
+    var press = document.getElementsByClassName("btn");
+    var message = document.getElementsByClassName("message");
+   
+
+    button.onclick = function(clicked) {
+        address = document.querySelector("input").value;
+        if(address.includes("@") && address.includes(".")) {
+            message[0].innerHTML = "Please enter a valid email address.";
+       
+        } else {
+            message[0].innerHTML = "Thank you! Your email address " + " " + address+ " " + " has been added to our mailing list!";
+   
+        }
+    };
+})
+
